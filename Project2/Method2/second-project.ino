1525aa7329ea740c2a43a9b89304c91731c96c59
octominus <korkmazu16@itu.edu.tr>
2020-10-15 16:31:07 +0300

    Changed second-project code

--- Project2/Method2/second-project.ino
+++ Project2/Method2/second-project.ino
@@ -1,18 +1,23 @@
+#define shiftLeft (PORTD << 1)
 bool buttonState = 0;
+byte LED = B00010000;
 
 void setup()
 {
   	DDRD = B11110000;
-  	PORTD = B00010000;
+  	PORTD = B00000000;
 }
 
 void loop()
 {
   if(((PIND & B00001000) == 8) && !buttonState) {
     buttonState = 1;
-    PORTD = PORTD << 1;
-    if(PORTD == B00000000) {
-    	PORTD = B00010000;
+    PORTD = PORTD ^ LED;
+    LED = (LED << 1);
+    if(PORTD == B11110000) {
+      	LED = B00010000;
+    } else if (PORTD == B00000000) {
+    	LED = B00010000;
     }
   } else if ((PIND & B00001000) == 0) {
   	buttonState = 0;
